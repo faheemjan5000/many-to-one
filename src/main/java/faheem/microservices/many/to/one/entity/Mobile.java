82bceac85ad631c9a62ec91e223dc8feaa3c224a
faheemjan5000 <faheemjan500@gmail.com>
2022-05-31 22:08:26 +0200

    entities added

---
+++ src/main/java/faheem/microservices/many/to/one/entity/Mobile.java
@@ -0,0 +1,30 @@
+package faheem.microservices.many.to.one.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Mobile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer mobileId;
+    private String mobileName;
+    private String processorName;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            name = "person_id",
+            referencedColumnName = "personId"
+    )
+    private Person person;
+}
